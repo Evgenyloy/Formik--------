fecb68916e7622d3eb087c9ad1d00ee9bf48a3c9
Evgeny Loy <evgenyloy@gmail.com>
2023-11-07 22:19:34 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './app.scss';
+import Form from './Form';
+import CusstomForm from './FormComponent';
+
+function App() {
+  return (
+    <div className="app">
+      {/* <Form /> */}
+      <CusstomForm />
+    </div>
+  );
+}
+
+export default App;
